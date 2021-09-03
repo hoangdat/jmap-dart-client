5bb69649c8e6c7ac649fd08dbf39f9e3c7012e66
dab246 <tdvu@linagora.com>
2021-09-03 23:40:37 +0700

    fixup! TF-50 Add unit test for set email submission

--- test/jmap/email/submission/set_email_submission_method_test.dart
+++ test/jmap/email/submission/set_email_submission_method_test.dart
@@ -1,23 +1,38 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
+import 'package:http_parser/http_parser.dart';
+import 'package:jmap_dart_client/http/converter/mailbox_id_converter.dart';
 import 'package:jmap_dart_client/http/http_client.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/core/patch_object.dart';
-import 'package:jmap_dart_client/jmap/core/reference_id.dart';
+import 'package:jmap_dart_client/jmap/core/reference_prefix.dart';
+import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
 import 'package:jmap_dart_client/jmap/jmap_request.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email_address.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email_body_part.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email_body_value.dart';
+import 'package:jmap_dart_client/jmap/mail/email/reference_email_id.dart';
+import 'package:jmap_dart_client/jmap/mail/email/set/set_email_method.dart';
+import 'package:jmap_dart_client/jmap/mail/email/set/set_email_response.dart';
 import 'package:jmap_dart_client/jmap/mail/email/submission/address.dart';
 import 'package:jmap_dart_client/jmap/mail/email/submission/email_submission.dart';
+import 'package:jmap_dart_client/jmap/mail/email/submission/email_submission_id.dart';
 import 'package:jmap_dart_client/jmap/mail/email/submission/envelope.dart';
+import 'package:jmap_dart_client/jmap/mail/email/submission/reference_email_submission_id.dart';
 import 'package:jmap_dart_client/jmap/mail/email/submission/set/set_email_submission_method.dart';
-import 'package:jmap_dart_client/jmap/mail/email/submission/set/set_email_submission_response.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 
 void main() {
   group('test to json set email submission method', () {
-    final expectedCreated =
-        EmailSubmission(id: Id('84c73cc3-4480-4ff9-a031-ff324ea36d7e'));
+    final expectedCreated = Email(
+        EmailId(Id('dada4400-0c7f-11ec-b57c-2fef1ee78d9e')),
+        blobId: Id('dada4400-0c7f-11ec-b57c-2fef1ee78d9e'),
+        threadId: ThreadId(Id('dada4400-0c7f-11ec-b57c-2fef1ee78d9e')),
+        size: UnsignedInt(662)
+    );
 
     test('set email submission method and response parsing', () async {
       final baseOption = BaseOptions(method: 'POST');
@@ -29,112 +44,183 @@ void main() {
                 "sessionState": "2c9f1b12-b35a-43e6-9af2-0106fb53a943",
                 "methodResponses": [
                   [
-                    "EmailSubmission/set",
+                    "Email/set",
                     {
                       "accountId":
                           "3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12",
-                      "newState": "2c9f1b12-b35a-43e6-9af2-0106fb53a943",
+                      "oldState": "b5e22550-0c70-11ec-b57c-2fef1ee78d9e",
+                      "newState": "b5e22550-0c70-11ec-b57c-2fef1ee78d9e",
                       "created": {
                         "dab1234": {
-                          "id": "84c73cc3-4480-4ff9-a031-ff324ea36d7e"
+                          "id": "dada4400-0c7f-11ec-b57c-2fef1ee78d9e",
+                          "blobId": "dada4400-0c7f-11ec-b57c-2fef1ee78d9e",
+                          "threadId": "dada4400-0c7f-11ec-b57c-2fef1ee78d9e",
+                          "size": 662
                         }
                       }
                     },
                     "c0"
                   ],
                   [
+                    "EmailSubmission/set",
+                    {
+                      "accountId":
+                          "3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12",
+                      "newState": "2c9f1b12-b35a-43e6-9af2-0106fb53a943",
+                      "created": {
+                        "a1234": "3126bf86-1ca0-4307-a789-3c7bf6575e82"
+                      }
+                    },
+                    "c1"
+                  ],
+                  [
                     "Email/set",
                     {
                       "accountId":
                           "3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12",
-                      "oldState": "9a333e40-0a25-11ec-b57c-2fef1ee78d9e",
-                      "newState": "9a333e40-0a25-11ec-b57c-2fef1ee78d9e",
-                      "updated": {"eb519670-05cd-11ec-b153-2fef1ee78d9e": null}
+                      "oldState": "db19be00-0c7f-11ec-b57c-2fef1ee78d9e",
+                      "newState": "db19be00-0c7f-11ec-b57c-2fef1ee78d9e",
+                      "updated": {"dada4400-0c7f-11ec-b57c-2fef1ee78d9e": null}
                     },
-                    "c0"
+                    "c1"
                   ]
                 ]
               }),
           data: {
+            "using": [
+              "urn:ietf:params:jmap:submission",
+              "urn:ietf:params:jmap:mail",
+              "urn:ietf:params:jmap:core"
+            ],
             "methodCalls": [
+              [
+                "Email/set",
+                {
+                  "accountId": "3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12",
+                  "create": {
+                    "dab1234": {
+                      "id": "dab1234",
+                      "mailboxIds": {
+                        "5dfb3290-0a14-11ec-b57c-2fef1ee78d9e": true
+                      },
+                      "subject": "test send email",
+                      "from": [
+                        {
+                          "name": "userB",
+                          "email": "userb@qa.open-paas.org"
+                        }
+                      ],
+                      "to": [
+                        {
+                          "name": "userD",
+                          "email": "userd@qa.open-paas.org"
+                        }
+                      ],
+                      "htmlBody": [
+                        {"partId": "mmm", "blobId": "aaaa", "type": "text/html"}
+                      ],
+                      "bodyValues": {
+                        "mmm": {
+                          "value": "<!DOCTYPE html> <html> <body> <p><b>Hello test send 2</b></p><br><br></body> </html>",
+                          "isEncodingProblem": false,
+                          "isTruncated": false
+                        }
+                      }
+                    }
+                  }
+                },
+                "c0"
+              ],
               [
                 "EmailSubmission/set",
                 {
-                  "accountId":
-                      "3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12",
+                  "accountId": "3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12",
                   "create": {
-                    "dab1234": {
-                      "identityId": "a123456",
-                      "emailId": "eb519670-05cd-11ec-b153-2fef1ee78d9e",
+                    "a1234": {
+                      "emailId": "#dab1234",
                       "envelope": {
-                        "mailFrom": {
-                          "email": "userb@qa.open-paas.org",
-                          "parameters": null
-                        },
+                        "mailFrom": {"email": "userb@qa.open-paas.org"},
                         "rcptTo": [
-                          {
-                            "email": "usera@qa.open-paas.org",
-                            "parameters": null
-                          }
+                          {"email": "userd@qa.open-paas.org"}
                         ]
                       }
                     }
                   },
                   "onSuccessUpdateEmail": {
-                    "#dab1234": {
-                      "mailboxIds/abc66d50-18d9-11eb-a677-2990b970028d": null,
-                      "mailboxIds/abb99c10-18d9-11eb-a677-2990b970028d": true,
-                      "keywords/\$draft": null
+                    "#a1234": {
+                      "mailboxIds": {
+                        "5dfb3290-0a14-11ec-b57c-2fef1ee78d9e": true
+                      }
                     }
                   }
                 },
-                "c0"
+                "c1"
               ]
-            ],
-            "using": [
-              "urn:ietf:params:jmap:submission",
-              "urn:ietf:params:jmap:mail",
-              "urn:ietf:params:jmap:core"
             ]
           },
           headers: {
-            "accept": "application/json;jmapVersion=rfc-8621"
+            "accept": "application/json;jmapVersion=rfc-8621",
+            "content-type": "application/json; charset=utf-8",
+            "content-length": 1039
           });
 
-      final setEmailSubmissionMethod = SetEmailSubmissionMethod(AccountId(Id(
-          '3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12')))
+      final setEmailMethod = SetEmailMethod(AccountId(Id('3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12')))
+        ..addCreate(Id('dab1234'),
+            Email(
+              EmailId(Id('dab1234')),
+              mailboxIds: {MailboxId(Id('5dfb3290-0a14-11ec-b57c-2fef1ee78d9e')): true},
+              from: {EmailAddress("userB", 'userb@qa.open-paas.org')},
+              to: {EmailAddress("userD", 'userd@qa.open-paas.org')},
+              subject: 'test send email',
+              htmlBody: {EmailBodyPart(partId: PartId('mmm'), blobId: Id('aaaa'), type: MediaType.parse('text/html'))},
+              bodyValues: {
+                PartId('mmm'): EmailBodyValue('<!DOCTYPE html> <html> <body> <p><b>Hello test send 2</b></p><br><br></body> </html>', false, false)
+              }
+            )
+        );
+
+      final setEmailSubmissionMethod = SetEmailSubmissionMethod(AccountId(Id('3ce33c876a726662c627746eb9537a1d13c2338193ef27bd051a3ce5c0fe5b12')))
         ..addCreate(
-            Id('dab1234'),
+            Id('a1234'),
             EmailSubmission(
-                identityId: Id('a123456'),
-                emailId: EmailId(Id('eb519670-05cd-11ec-b153-2fef1ee78d9e')),
-                envelope: Envelope(Address('userb@qa.open-paas.org', null),
-                    {Address('usera@qa.open-paas.org', null)})))
+                emailId: ReferenceEmailId(ReferencePrefix.defaultPrefix, EmailId(Id('dab1234'))),
+                envelope: Envelope(
+                    Address('userb@qa.open-paas.org'),
+                    {Address('userd@qa.open-paas.org')})))
         ..addOnSuccessUpdateEmail({
-          ReferenceId('#', Id('dab1234')): PatchObject({
-            "mailboxIds/abc66d50-18d9-11eb-a677-2990b970028d": null,
-            "mailboxIds/abb99c10-18d9-11eb-a677-2990b970028d": true,
-            "keywords/\$draft": null
-          }).patches
+          ReferenceEmailSubmissionId(ReferencePrefix.defaultPrefix, EmailSubmissionId(Id('a1234'))): PatchObject({
+            PatchObject.mailboxIdsProperty: {
+              MailboxIdConverter().toJson(MailboxId(Id('5dfb3290-0a14-11ec-b57c-2fef1ee78d9e'))): true
+            }
+          })
         });
 
       final httpClient = HttpClient(dio);
       final requestBuilder =
           JmapRequestBuilder(httpClient, ProcessingInvocation());
-      final setEmailInvocation =
-          requestBuilder.invocation(setEmailSubmissionMethod);
+
+      final setEmailInvocation = requestBuilder.invocation(setEmailMethod);
+
+      final setEmailSubmissionInvocation = requestBuilder.invocation(setEmailSubmissionMethod);
+
       final response = await (requestBuilder
             ..usings(setEmailSubmissionMethod.requiredCapabilities))
           .build()
           .execute();
 
-      final setEmailSubmissionResponse =
-          response.parse<SetEmailSubmissionResponse>(
-              setEmailInvocation.methodCallId,
-              SetEmailSubmissionResponse.deserialize);
+      final setEmailResponse = response.parse<SetEmailResponse>(
+          setEmailInvocation.methodCallId,
+          SetEmailResponse.deserialize);
 
-      expect(setEmailSubmissionResponse?.created?[Id('dab1234')],
+      final setEmailUpdateResponse = response.parse<SetEmailResponse>(
+          setEmailSubmissionInvocation.methodCallId,
+          SetEmailResponse.deserialize);
+
+      expect(setEmailResponse?.created?[Id('dab1234')],
           equals(expectedCreated));
+
+      expect(setEmailUpdateResponse?.updated?[Id('dada4400-0c7f-11ec-b57c-2fef1ee78d9e')],
+          equals(null));
     });
   });
 }
